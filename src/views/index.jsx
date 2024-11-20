b00d4f5be7b0bee3e7dc635b879a5ed94d58bb94
tanwei <674967815@qq.com>
2024-11-20 10:23:07 +0800

    update -views

---
+++ src/views/index.jsx
@@ -0,0 +1,10 @@
+import { ModuleList } from "../components/List"
+import { DragUpload } from "../components/DragUpload"
+export default function Index() {
+  return (
+    <>
+      <ModuleList />
+      {/* <DragUpload /> */}
+    </>
+  )
+}
