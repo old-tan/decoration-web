8bbd2dab73dc083b212972d25db441190ac0027b
tanwei <674967815@qq.com>
2024-11-20 10:23:38 +0800

    extra content

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,14 @@
-import React, { useState, useEffect } from "react"
-import { Button, Form, Input, message, Upload } from "antd"
-import { InboxOutlined } from "@ant-design/icons"
-import axios from "axios"
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query"
+import RootRoute from "./router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 // Create a client
 const queryClient = new QueryClient()
 
-import { getList, postItem } from "./api"
-
-const { Dragger } = Upload
-// import axios from "axios"
 const App = () => {
-  // upload file
-  const props = {
-    name: "lod",
-    multiple: false,
-    action: "http://localhost:3031/uploads",
-    onChange(info) {
-      const { status } = info.file
-      if (status !== "uploading") {
-        console.log(info.file, info.fileList)
-      }
-      if (status === "done") {
-        message.success(`${info.file.name} file uploaded successfully.`)
-      } else if (status === "error") {
-        message.error(`${info.file.name} file upload failed.`)
-      }
-    },
-    onDrop(e) {
-      console.log("Dropped files", e.dataTransfer.files)
-    },
-  }
   return (
     <QueryClientProvider client={queryClient}>
-      <List />
-
-      <Dragger {...props}>
-        <p className="ant-upload-drag-icon">
-          <InboxOutlined />
-        </p>
-        <p className="ant-upload-text">
-          Click or drag file to this area to upload
-        </p>
-        <p className="ant-upload-hint">
-          Support for a single or bulk upload. Strictly prohibited from
-          uploading company data or other banned files.
-        </p>
-      </Dragger>
+      <RootRoute />
     </QueryClientProvider>
   )
 }
-// get list
-function List() {
-  const [form] = Form.useForm()
 
-  const { isPending, error, data, isFetching } = useQuery({
-    queryKey: ["list"],
-    queryFn: getList,
-  })
-
-  // Mutations
-  const mutation = useMutation({
-    mutationFn: postItem,
-    onSuccess: () => {
-      // Invalidate and refetch
-      queryClient.invalidateQueries({ queryKey: ["list"] })
-    },
-  })
-
-  const onFinish = (value) => {
-    console.log("value---", value)
-    mutation.mutate(value)
-  }
-
-  return (
-    <>
-      <ul>
-        {data &&
-          data.map((item) => (
-            <li key={item.id}>
-              <p>{item.id}</p>
-              <p>{item.lod0}</p>
-              <p>{item.lod1}</p>
-              <p>{item.lod2}</p>
-              <p>{item.thumb}</p>
-            </li>
-          ))}
-      </ul>
-      <Form layout="vertical" form={form} onFinish={onFinish}>
-        <Form.Item name="lod0" label="lod0">
-          <Input placeholder="input placeholder" />
-        </Form.Item>
-        <Form.Item name="lod1" label="lod1">
-          <Input placeholder="input placeholder" />
-        </Form.Item>
-        <Form.Item name="lod2" label="lod2">
-          <Input placeholder="input placeholder" />
-        </Form.Item>
-        <Form.Item name="thumb" label="thumb">
-          <Input placeholder="input placeholder" />
-        </Form.Item>
-        <Form.Item name="photo" label="photo">
-          <Input placeholder="input placeholder" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </>
-  )
-}
 export default App
--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,15 @@
 import axios from "axios"
 export const getList = async () => {
-  const res = await axios.get("http://localhost:3030/uploads")
+  const res = await axios.get("http://localhost:3030/models")
   console.log("res---", res)
   return await res.data.data
 }
 
 export const postItem = async (data) => {
-  const res = await axios.post("http://localhost:3030/uploads", { ...data })
+  const res = await axios.post("http://localhost:3030/models", { ...data })
   console.log("res---", res)
+}
+
+export const uploadFile = async () => {
+  // const res = await axios.post("http://localhost:3030/models", { ...data })
 }
