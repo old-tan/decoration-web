b35a01591e79b899f6bcb14c565d64f47c4f57cb
tanwei <674967815@qq.com>
2024-11-28 17:50:33 +0800

    update router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,4 +1,9 @@
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom"
+import {
+  createBrowserRouter,
+  BrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom"
 import { lazy } from "react"
 import { Spin } from "antd"
 
@@ -6,40 +11,57 @@ const Index = lazy(() => import("@/views/index"))
 // const NotFound = lazy(() => import("@/views/notfound"))
 
 const modules = import.meta.glob("@/views/*/*.jsx")
-console.log("modules---", modules)
+// console.log("modules---", modules)
 const components = Object.keys(modules).reduce((prev, cur) => {
   prev[cur.replace("@/views", "")] = modules[cur]
   return prev
 }, {})
 
-console.log("components---", components)
+// console.log("components---", components)
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Index,
+      children: [
+        // {
+        //   path: "/",
+        //   element: <Navigate to="/home" />,
+        // },
+        // {
+        //   path: "/home",
+        //   label: "一级菜单-1",
+        //   Component: lazy(() => import("@/views/Home")),
+        // },
+      ],
+    },
+    // {
+    //   path: "*",
+    //   Component: NotFound,
+    // },
+  ],
   {
-    path: "/",
-    Component: Index,
-    children: [
-      // {
-      //   path: "/",
-      //   element: <Navigate to="/home" />,
-      // },
-      // {
-      //   path: "/home",
-      //   label: "一级菜单-1",
-      //   Component: lazy(() => import("@/views/Home")),
-      // },
-    ],
-  },
-  // {
-  //   path: "*",
-  //   Component: NotFound,
-  // },
-])
+    future: {
+      v7_fetcherPersist: true,
+      v7_relativeSplatPath: true,
+      v7_startTransition: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
+)
 
 export default function RootRoute() {
   return (
     // <RouterContext.Provider value={{ menus }}>
-    <RouterProvider router={router} />
+    <RouterProvider
+      future={{
+        v7_startTransition: true,
+      }}
+      router={router}
+    />
     // </RouterContext.Provider>
   )
 }
