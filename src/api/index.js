18da65a8336be51a6546a4e0185287b80d552038
tanwei <674967815@qq.com>
2024-11-28 17:47:33 +0800

    update api methods

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,41 @@
 import axios from "axios"
-export const getList = async () => {
-  const res = await axios.get("http://localhost:3030/models")
-  console.log("res---", res)
-  return await res.data.data
+import request from "@/service/request"
+
+// getList
+export const getList = async ({ page, limit }) => {
+  const res = await request.get("/models", {
+    $skip: (page - 1) * limit,
+    $limit: limit,
+  })
+  return res.data
 }
 
+// create
 export const postItem = async (data) => {
-  const res = await axios.post("http://localhost:3030/models", { ...data })
-  console.log("res---", res)
+  const res = await request.post("/models", data)
+  return res
 }
 
-export const uploadFile = async () => {
-  // const res = await axios.post("http://localhost:3030/models", { ...data })
+// update
+export const updateItem = async ({ id, data }) => {
+  const res = await request.put(`/models/${id}`, data)
+  return res
+}
+
+//delete
+export const deleteItem = async (id) => {
+  const res = await request.delete(`/models/${id}`)
+  return res
+}
+
+// upload
+export const postModelFiles = async (data) => {
+  const res = await request.post("/model-files", data)
+  return res
+}
+
+// getAttr
+export const getAttributes = async () => {
+  const res = await request.get("/attributes")
+  return res.data
 }
