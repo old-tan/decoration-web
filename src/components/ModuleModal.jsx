695dd60d4d19fad9bf6a75e99441f01d371f1099
tanwei <674967815@qq.com>
2024-11-28 17:45:33 +0800

    update module-modal

--- src/components/ModuleModal.jsx
+++ src/components/ModuleModal.jsx
@@ -1,20 +1,27 @@
-import React, { useState } from "react"
+import React, { useEffect } from "react"
 import { Button, Flex, Form, Input, Select, Modal, Upload, message } from "antd"
-import axios from "axios"
 const { Dragger } = Upload
-import { InboxOutlined, UploadOutlined } from "@ant-design/icons"
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query"
+import { InboxOutlined } from "@ant-design/icons"
 
-import { getList, postItem } from "../api"
-const queryClient = new QueryClient()
+import { useMutation } from "@tanstack/react-query"
+
+import { postItem, updateItem } from "../api"
+import { useImmer } from "use-immer"
+
 export function ModuleModal(props) {
-  const { isModalOpen, getOpen, getMute } = props
+  const { isModalOpen, getOpen, detailData, onSuccess, isCreate, attrList } =
+    props
+  const {
+    id,
+    name: model_name,
+    tags: detail_tags,
+    files: modelFiles,
+  } = detailData
+  const [tags, updateTags] = useImmer([])
+  const [isNextOpen, updateIsNextOpen] = useImmer(false)
+  const [fileList, updateFileList] = useImmer([])
+  // const [thumbs, updateThumbs] = useImmer([])
+
   const [form] = Form.useForm()
   const normFile = (e) => {
     console.log("Upload event:", e)
@@ -24,47 +31,123 @@ export function ModuleModal(props) {
     return e?.fileList
   }
 
+  // set form fields
+  const editTags = detail_tags && detail_tags.map((item) => item.value)
+  useEffect(() => {
+    if (isCreate) {
+      form.resetFields()
+    } else {
+      updateFileList((draft) => {
+        return modelFiles.map((file) => ({
+          uid: file.id,
+          model_id: file.model_id,
+          name: file.file_name,
+          uri: file.url,
+          status: "done",
+        }))
+      })
+      form.setFieldsValue({ name: model_name, tags: [...editTags] })
+    }
+  }, [isCreate])
+
+  const handleChange = (value) => {
+    updateTags(() => value)
+  }
   // Mutations
   const mutation = useMutation({
     mutationFn: postItem,
     onSuccess: () => {
-      getMute(true)
-      // Invalidate and refetch
-      // queryClient.invalidateQueries({ queryKey: ["list"] })
+      onSuccess()
+      getOpen(false)
     },
   })
 
-  const onFinish = (values) => {
-    console.log("values---", values)
-    mutation.mutate(values)
-  }
+  const updateMutation = useMutation({
+    mutationFn: updateItem,
+    onSuccess: (res) => {
+      onSuccess()
+      getOpen(false)
+    },
+  })
 
-  // const [isModalOpen, setIsModalOpen] = useState(false)
-  // const showModal = () => {
-  //   setIsModalOpen(true)
-  // }
+  // next step
+  const handleNext = () => {
+    if (!isNextOpen) {
+      form
+        .validateFields()
+        .then((values) => {
+          updateIsNextOpen((draft) => true)
+        })
+        .catch((errorInfo) => {
+          console.log("errorInfo---", errorInfo)
+        })
+    } else {
+      updateIsNextOpen((draft) => false)
+    }
+  }
   const handleOk = () => {
-    // setIsModalOpen(false)
     getOpen(false)
   }
   const handleCancel = () => {
-    // setIsModalOpen(false)
     getOpen(false)
   }
 
-  const uploadProps = {
+  // handle submit
+  const handleSubmit = () => {
+    form
+      .validateFields()
+      .then((values) => {
+        const uploads = fileList.map((item) => {
+          const { name, uri } = item
+          return {
+            file_name: name,
+            url: uri,
+          }
+        })
+        if (isCreate) {
+          // create
+          const submitValues = {
+            ...values,
+            uploads,
+          }
+          mutation.mutate(submitValues)
+        } else {
+          // update
+          const updateValues = {
+            ...detailData,
+            ...values,
+            uploads,
+          }
+          updateMutation.mutate({ id, data: updateValues })
+        }
+      })
+      .catch((errorInfo) => {
+        console.log("errorInfo---", errorInfo)
+      })
+  }
+
+  const uploadFileProps = {
     name: "uri",
     multiple: false,
-    action: "http://localhost:3030/upload-files",
+    maxCount: 1,
+    defaultFileList: fileList,
+    action: `${import.meta.env.VITE_APP_BASE_URL}/upload-files`,
     onChange(info) {
-      const { status } = info.file
-      if (status !== "uploading") {
-        console.log(info.file, info.fileList)
+      const { file, fileList: currentFileList } = info
+      if (file.status === "done") {
+        if (info.file.response) {
+          const newList = currentFileList.map((item) => {
+            return {
+              ...item,
+              uri: item.response.uri,
+            }
+          })
+          updateFileList(() => newList)
+        }
       }
-      if (status === "done") {
-        message.success(`${info.file.name} file uploaded successfully.`)
-      } else if (status === "error") {
-        message.error(`${info.file.name} file upload failed.`)
+      // removed: clear fileList
+      if (file.status === "removed") {
+        updateFileList([])
       }
     },
     onDrop(e) {
@@ -74,17 +157,33 @@ export function ModuleModal(props) {
 
   return (
     <>
-      {/* <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button> */}
       <Modal
         forceRender
-        title="Basic Modal"
+        title={isCreate ? "Create New Model" : "Edit Model"}
         width={1000}
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
-        footer
+        footer={
+          // <Button key="cancel" onClick={handleCancel}>
+          //   cancel
+          // </Button>,
+          <>
+            {!isNextOpen && (
+              <Button key="next" type="default" onClick={handleNext}>
+                next
+              </Button>
+            )}
+            {isNextOpen && (
+              <Button key="submit" type="primary" onClick={handleSubmit}>
+                {isCreate ? "Submit" : "Update"}
+              </Button>
+            )}
+            {/* <Button key="submit" type="primary" onClick={handleSubmit}>
+              {isCreate ? "Submit" : "Update"}
+            </Button> */}
+          </>
+        }
       >
         <Form
           form={form}
@@ -98,7 +197,6 @@ export function ModuleModal(props) {
           wrapperCol={{
             span: 20,
           }}
-          onFinish={onFinish}
         >
           <Form.Item
             name="name"
@@ -111,49 +209,78 @@ export function ModuleModal(props) {
           >
             <Input />
           </Form.Item>
-          <Form.Item label="Dragger">
-            <Form.Item
-              // name="dragger"
-              valuePropName="fileList"
-              getValueFromEvent={normFile}
-              noStyle
-            >
-              <Flex justify="space-between">
-                <Flex align="center" gap="middle" vertical>
-                  <Select
-                    options={[{ value: "sample", label: <span>sample</span> }]}
-                  />
-                  <Dragger {...uploadProps}>
-                    <p className="ant-upload-drag-icon">
-                      <InboxOutlined />
-                    </p>
-                    <p className="ant-upload-text">
-                      Click or drag file to this area to upload
-                    </p>
-                    <p className="ant-upload-hint">
-                      Support for a single or bulk upload. Strictly prohibited
-                      from uploading company data or other banned files.
-                    </p>
-                  </Dragger>
-                </Flex>
-              </Flex>
-            </Form.Item>
-          </Form.Item>
           <Form.Item
-            wrapperCol={{
-              offset: 10,
-            }}
+            name="tags"
+            label="Tags"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
           >
-            <Flex gap="small">
-              <Button danger onClick={() => form.resetFields()}>
-                Reset
-              </Button>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Flex>
+            <Select
+              mode="tags"
+              value={tags}
+              style={{ width: "100%" }}
+              placeholder="multiple Mode"
+              onChange={handleChange}
+              options={attrList}
+            />
           </Form.Item>
         </Form>
+        {isNextOpen && (
+          <Form
+            form={form}
+            scrollToFirstError
+            style={{
+              paddingBlock: 32,
+            }}
+            labelCol={{
+              span: 3,
+            }}
+            wrapperCol={{
+              span: 20,
+            }}
+          >
+            {/* <Form.Item label="UploadThumb">
+              <Form.Item getValueFromEvent={normFile} noStyle>
+                <Dragger
+                  name="uri"
+                  multiple={false}
+                  maxCount={1}
+                  defaultFileList={thumbs}
+                  action={`${import.meta.env.VITE_APP_BASE_URL}/upload-files`}
+                  onChange={(info) => {
+                    const { file, fileList: currentFileList } = info
+                    if (file.status === "done") {
+                      if (info.file.response) {
+                        updateThumbs(() => currentFileList)
+                      }
+                    }
+                  }}
+                  onDrop={(e) => {
+                    console.log("Dropped files", e.dataTransfer.files)
+                  }}
+                >
+                  <p className="ant-upload-drag-icon">
+                    <InboxOutlined />
+                  </p>
+                  <p className="ant-upload-text">Click or drag file</p>
+                </Dragger>
+              </Form.Item>
+            </Form.Item> */}
+            <Form.Item label="UploadModel">
+              <Form.Item getValueFromEvent={normFile} noStyle>
+                <Dragger {...uploadFileProps}>
+                  <p className="ant-upload-drag-icon">
+                    <InboxOutlined />
+                  </p>
+                  <p className="ant-upload-text">Click or drag file</p>
+                </Dragger>
+              </Form.Item>
+            </Form.Item>
+          </Form>
+        )}
       </Modal>
     </>
   )
